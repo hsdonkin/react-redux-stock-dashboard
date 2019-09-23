273d9524d19f2e5414cf147709b621e21c1b10e2
Student <example@example.com>
2019-09-23 09:33:41 -0700

    cleanin it up 4 josh

--- src/App.jsx
+++ src/App.jsx
@@ -13,58 +13,10 @@ class App extends React.Component {
     this.state = { key: v4() };
   }
 
-  callAlphaVantageApi = () => {
-    console.log("Calling API");
-    fetch(
-      `https://www.alphavantage.co/query?function=BATCH_STOCK_QUOTES&apikey=${
-        process.env["API_KEY"]
-      }&symbols=${this.props.state.symbols.join()}`
-    )
-      .then(res => res.json())
-      .then(response => {
-        console.log("API Response: ", response);
-        this.props.dispatch(loadStocks(response));
-      });
-  };
-
-  handleNewStockSymbol = () => {
-    this.callAlphaVantageApi();
-    this.setState({ key: v4() });
-  };
-
-  componentWillMount = () => {
-    this.callAlphaVantageApi();
-  };
-
-  // call the API and get the data when App enters the dom
-  componentDidMount = () => {
-    this.apiCallTimer = setInterval(
-      () => this.callAlphaVantageApi(),
-      1000 * 60 // 60 seconds
-    );
-  };
-
-  componentWillUnmount = () => {
-    clearInterval(this.apiCallTimer);
-  };
-
   render() {
-    let loader;
-    if (this.props.state.loaded === false) {
-      loader = <h1>Loading...</h1>;
-    } else if (this.props.state.apiData.loaded === true) {
-      loader = <h1>Loaded!!</h1>;
-    }
-
     return (
       <BrowserRouter>
-        <div>
-          {loader}
-          <StockList
-            key={this.state.key}
-            onNewStockSymbol={this.handleNewStockSymbol}
-          />
-        </div>
+        <h1>App is working</h1>
       </BrowserRouter>
     );
   }
