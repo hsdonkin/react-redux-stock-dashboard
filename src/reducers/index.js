fe9ed1e1bf7fd7454db6959e646bba5881fe23fa
Student <example@example.com>
2019-09-23 09:57:59 -0700

    remove reducers is broken

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,3 @@
 import { combineReducers } from "redux";
-import apiData from "./api.js";
-import symbols from "./symbols.js";
 
-export default combineReducers({
-  apiData,
-  symbols
-});
+export default combineReducers({});
